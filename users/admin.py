a968cbe46890c51f2dacc15de851484581af956f
kooshachehreh <koosha.bagherzadeh@gmail.com>
2025-01-26 21:09:27 +0330

    user admin registered

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "username",
+        "phone",
+        "password",
+        "created_at",
+        "updated_at",
+        "suspended_at",
+    ]
+    list_filter = [
+        "created_at",
+    ]
+    search_fields = ["username", "phone", "id"]
